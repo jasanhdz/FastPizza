61a91b9e6bf331b19a89779255357ff91769f948
Jasan Hernandez <jasan_azael@hotmail.es>
2020-04-27 13:34:50 -0500

    receiving messages from messenger

--- app.js
+++ app.js
@@ -10,14 +10,25 @@ app.set('port', 5000);
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World Jasan!');
 });
+
 app.get('/webhook', (req, res) => {
   if (req.query['hub.verify_token'] === 'fastpizza_token') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Fast Pizza no tienes permisos');
   }
+});
+
+app.post('/webhook', (req, res) => {
+  const webhook_event = req.body.entry[0];
+  if (webhook_event.messaging) {
+    webhook_event.messaging.forEach((event) => {
+      console.log(event);
+    });
+  }
+  res.sendStatus(200);
 });
 
 app.listen(app.get('port'), () =>

adf52bfcbfb5c6f1360de1dbd44dd3775aae62af
Jasan Hernandez <jasan_azael@hotmail.es>
2020-04-28 15:11:47 -0500

    Template:List of elements

--- app.js
+++ app.js
@@ -48,9 +48,12 @@ const handleMessage = (senderId, event) => {
 };
 
 const handlePostback = (senderId, { payload }) => {
+  console.log(payload);
   switch (payload) {
     case 'GET_STARTED_FASTPIZZA':
       return console.log('entro al GET_STARTED_FASTPIZZA');
+    case 'PIZZAS_PAYLOAD':
+      return showPizzas(senderId);
   }
 };
 
@@ -101,6 +104,65 @@ const defaultMessage = (senderId) => {
     },
   };
   senderActions(senderId);
+  callSendApi(messageData);
+};
+
+const showPizzas = (senderId) => {
+  const messageData = {
+    recipient: {
+      id: senderId,
+    },
+    message: {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [
+            {
+              title: 'Pepperoni',
+              subtitle: 'Con todo el sabor del peperoni',
+              image_url:
+                'https://olodominos.blob.core.windows.net/dev/webOptimized/especialidad/PES/PES.png',
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Elegir Pepperoni',
+                  payload: 'PEPPERONI_PAYLOAD',
+                },
+              ],
+            },
+            {
+              title: 'Pollo BBQ',
+              subtitle: 'Con todo el sabor del BBQ',
+              image_url:
+                'https://olodominos.blob.core.windows.net/dev/webOptimized/especialidad/CHIHNC/CHIHNC.png',
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Elegir Pollo BBQ',
+                  payload: 'CHICKEN_BBQ_PAYLOAD',
+                },
+              ],
+            },
+            {
+              title: 'Hawaiana',
+              subtitle: 'Con todo el sabor de la piña',
+              image_url:
+                'https://olodominos.blob.core.windows.net/dev/webOptimized/especialidad/HNC/HNC.png',
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Elegir Hawaiana',
+                  payload: 'HAWAIANA_PAYLOAD',
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  };
+
   callSendApi(messageData);
 };
 
